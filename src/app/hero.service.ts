e0f0ad6e11320a58f9a09d9a367056b48f659824
Alex Rabelo Ferreira <arf92@live.com>
2021-07-11 11:10:13 -0300

    update: service hero com add hero
    - add metodo para adicionar heroi

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -44,7 +44,7 @@ export class HeroService {
     const url = `${this.heroesUrl}/${id}`;
 
     const hero = this.http.get<Hero>(url).pipe(
-        tap(_ => this.log(`HeroService: fetched hero id=${id}`),
+        tap(_ => this.log(`fetched hero id=${id}`),
         catchError(this.handleError<Hero>(`getHero id =${id}`)))
       );
 
@@ -53,9 +53,16 @@ export class HeroService {
 
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap(_ => this.log(`HeroService: updated hero id=${hero.id}`),
+      tap(_ => this.log(`updated hero id=${hero.id}`),
         catchError(this.handleError<any>(`updateHero`)))
     );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`saved hero w/id=${newHero.id}`),
+        catchError(this.handleError<Hero>(`addHero`)))
+    )
   }
 
   log(message: string) {
